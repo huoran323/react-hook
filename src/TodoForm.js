dd9c70b1e1207088459bc2e4a9ac9df4ac6dc525
huoran <303176152@qq.com>
2019-06-23 22:38:14 +0800

    useReducer

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -1,41 +1,70 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
-// 抽取共通的方法
+// 改造后
+
 const useInputValue = initialValue => {
   const [value, setValue] = useState(initialValue);
 
   return {
     value,
-    onChange: e => setValue(e.target.value),
+    onChange: useCallback(e => setValue(e.target.value), []),
     resetValue: () => setValue("")
   };
 };
 
-// 抽取之后的
-export default ({ onSubmit }) => {
+export default React.memo(({ dispatch }) => {
+  console.log("rendering form");
   const { resetValue, ...text } = useInputValue("");
-
   return (
     <form
-      // onSubmit时，里面的方法会依次执行，可以是自定义的方法
       onSubmit={e => {
         e.preventDefault();
-        // 传入一个参数，调用父组件的onSubmit
-        onSubmit(text.value);
-        // 清空输入框内容
+        dispatch({ type: "ADD_TODO", text: text.value });
         resetValue();
       }}
     >
       <input type="text" {...text} />
     </form>
   );
-};
+});
 
-// 抽取之前的
-// export default () => {
-//   const [value, setValue] = useState("");
+// 改造前
+// // 抽取共通的方法
+// const useInputValue = initialValue => {
+//   const [value, setValue] = useState(initialValue);
+
+//   return {
+//     value,
+//     onChange: e => setValue(e.target.value),
+//     resetValue: () => setValue("")
+//   };
+// };
+
+// // 抽取之后的
+// export default ({ onSubmit }) => {
+//   const { resetValue, ...text } = useInputValue("");
 
 //   return (
-//     <input type="text" value={value} onChange={e => setValue(e.target.value)} />
+//     <form
+//       // onSubmit时，里面的方法会依次执行，可以是自定义的方法
+//       onSubmit={e => {
+//         e.preventDefault();
+//         // 传入一个参数，调用父组件的onSubmit
+//         onSubmit(text.value);
+//         // 清空输入框内容
+//         resetValue();
+//       }}
+//     >
+//       <input type="text" {...text} />
+//     </form>
 //   );
 // };
+
+// // 抽取之前的
+// // export default () => {
+// //   const [value, setValue] = useState("");
+
+// //   return (
+// //     <input type="text" value={value} onChange={e => setValue(e.target.value)} />
+// //   );
+// // };
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,43 +1,101 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useReducer } from "react";
 import TodoForm from "./TodoForm";
 
-const App = () => {
-  const [todos, setTodos] = useState([]);
+// 改造后
 
-  const toggleComplete = i =>
-    // 遍历todos,更新todo的completed
-    setTodos(
-      todos.map((todo, k) =>
-        k === i
+const todosReducer = (todos, action) => {
+  switch (action.type) {
+    case "ADD_TODO":
+      return [{ text: action.text, complete: false }, ...todos];
+    case "TOGGLE_COMPLETE":
+      return todos.map((todo, k) =>
+        k === action.i
           ? {
               ...todo,
-              completed: !todo.completed
+              complete: !todo.complete
             }
           : todo
-      )
-    );
+      );
+    case "RESET":
+      return [];
+    default:
+      return todos;
+  }
+};
+
+const App = () => {
+  // const [todos, setTodos] = useState([]);
+  const [todos, dispatch] = useReducer(todosReducer, []);
+
+  // const toggleComplete = (i) => setTodos(todos.map((todo, k) => k === i ? {
+  //   ...todo,
+  //   complete: !todo.complete
+  // } : todo))
+
+  // const onSubmit = useCallback(
+  //   text => setTodos([{ text, complete: false }, ...todos]),
+  //   [todos]
+  // )
+
   return (
     <div>
-      <TodoForm
-        // 传进来的text，添加到数组中
-        onSubmit={text => setTodos([{ text, completed: false }, ...todos])}
-      />
+      <TodoForm dispatch={dispatch} />
       <div>
-        {todos.map(({ text, completed }, i) => {
-          return (
-            <div
-              key={text}
-              onClick={() => toggleComplete(i)}
-              style={{ textDecoration: completed ? "line-through" : "" }}
-            >
-              {text}
-            </div>
-          );
-        })}
+        {todos.map(({ text, complete }, i) => (
+          <div
+            key={`${text}-${i}`}
+            onClick={() => dispatch({ type: "TOGGLE_COMPLETE", i })}
+            style={{ textDecoration: complete ? "line-through" : "" }}
+          >
+            {text}
+          </div>
+        ))}
       </div>
-      <button onClick={() => setTodos([])}>reset</button>
+      <button onClick={() => dispatch({ type: "RESET" })}>reset</button>
     </div>
   );
 };
 
 export default App;
+
+// 改造前
+// const App = () => {
+//   const [todos, setTodos] = useState([]);
+
+//   const toggleComplete = i =>
+//     // 遍历todos,更新todo的completed
+//     setTodos(
+//       todos.map((todo, k) =>
+//         k === i
+//           ? {
+//               ...todo,
+//               completed: !todo.completed
+//             }
+//           : todo
+//       )
+//     );
+//   return (
+//     <div>
+//       <TodoForm
+//         // 传进来的text，添加到数组中
+//         onSubmit={text => setTodos([{ text, completed: false }, ...todos])}
+//       />
+//       <div>
+//         {todos.map(({ text, completed }, i) => {
+//           return (
+//             <div
+//               key={text}
+//               onClick={() => toggleComplete(i)}
+//               style={{ textDecoration: completed ? "line-through" : "" }}
+//             >
+//               {text}
+//             </div>
+//           );
+//         })}
+//       </div>
+//       <button onClick={() => setTodos([])}>reset</button>
+//     </div>
+//   );
+// };
+
+// export default App;
